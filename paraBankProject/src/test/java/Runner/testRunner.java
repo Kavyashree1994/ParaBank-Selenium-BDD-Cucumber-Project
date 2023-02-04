9534b0873d7799018246ea47cc05e5bdc73f96a2
hp <hp@DESKTOP-PT0VKRK>
2023-02-04 15:38:09 +0530

    Changed feature file name

--- paraBankProject/src/test/java/Runner/testRunner.java
+++ paraBankProject/src/test/java/Runner/testRunner.java
@@ -6,14 +6,14 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber; 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/ParaBank-Login.feature",
-		"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/openNewAccount.feature",
-		"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/accountOverview.feature"},
+@CucumberOptions(features = {"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/1_ParaBank-Login.feature",
+		"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/2_openNewAccount.feature",
+		"C:/Users/hp/git/repository/paraBankProject/src/test/resources/Features/3_accountOverview.feature"},
 glue= {"stepDefinations"},
 dryRun=false,
 monochrome = true,
-plugin = {"pretty", "html:target/cucumber-reports/report2.json"}
-//plugin = {"pretty", "html:target/cucumber-reports/report.html"}
+//plugin = {"pretty", "html:target/cucumber-reports/report2.json"}
+plugin = {"pretty", "html:target/cucumber-reports/report.html"}
 )
 public class testRunner {
 	
